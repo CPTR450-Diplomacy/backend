464992e2e13a8e15923934737f29db7adb967492
Will Hensel <william.r.hensel@gmail.com>
2021-10-30 07:59:35 -0700

    WIP - Verofying JWTs

--- lib/controller/controller.dart
+++ lib/controller/controller.dart
@@ -1,6 +1,8 @@
 library controller;
 
+import 'dart:convert';
 import 'dart:io' show HttpServer, Platform;
+// import 'package:dart_jsonwebtoken/dart_jsonwebtoken.dart';
 import 'package:shelf/shelf.dart';
 import 'package:shelf/shelf_io.dart' as shelf_io;
 import 'package:shelf_router/shelf_router.dart';
--- lib/controller/login_controller.dart
+++ lib/controller/login_controller.dart
@@ -4,8 +4,15 @@ class LoginController {
   Router get router {
     final router = Router();
 
-    router.post('/', (Request r) {
-      print(r.readAsString());
+    router.post('/', (Request r) async {
+      var request = jsonDecode(await r.readAsString());
+      var token = request['sesionToken'];
+      var tokenIssuer = "https://securetoken.google.com/cptr450-diplomacy";
+      var env = Platform.environment;
+      // Map<String, String> credentials = jsonDecode(env['credentials']!);
+      String privateKey = env['private_key']!;
+      // final jwt = JWT.verify(token, SecretKey(privateKey));
+      // print(jwt.header);
       return Response.ok('Login post reponse');
     });
 
