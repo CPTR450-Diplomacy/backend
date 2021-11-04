979236fec6966b01f8e8ddb676f74cd513a564e3
Samuel Hernandez <samuel.hernandez@wallawalla.edu>
2021-11-03 21:37:19 -0700

    adding Unit class, extended classes

--- lib/model/unit.dart
+++ lib/model/unit.dart
@@ -1,2 +1,12 @@
 part of model;
 
+class Unit {
+  Order order = '' as Order;
+  Province position = '' as Province;
+  UnitType type  = '' as UnitType;
+}
+
+class UnitType {
+}
+
+// TODO link the order from order.dart to this file
