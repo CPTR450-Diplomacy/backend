59f85babf176250c974d0c62c2b1767e7b55a2c4
Samuel Hernandez <samuel.hernandez@wallawalla.edu>
2021-11-04 18:24:58 -0700

    adding player tests

--- test/model/player_test.dart
+++ test/model/player_test.dart
@@ -1,7 +1,16 @@
+import 'package:diplomacy/model/model.dart';
 import 'package:test/test.dart';
 
 
 void main(){
-  test('Player Test', () {});
+  test('Player Test', () {
+    Player player = Player();
+    expect(player.name, isA<String>());
+    expect(player.allegiances, <Country>(){
+      'Germany';
+      'Austria';
+      'Turkey';
+    });
+    
+  });
 }
-
