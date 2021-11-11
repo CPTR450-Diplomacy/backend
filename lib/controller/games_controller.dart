6375ab2839a6e0e2c14eadef8b239f3dd326b151
Stefan Hess <speterhess8@gmail.com>
2021-11-11 15:53:31 -0800

    starting to make :id get request handler

--- lib/controller/games_controller.dart
+++ lib/controller/games_controller.dart
@@ -13,6 +13,10 @@ class GamesController {
       return Response.ok("Games Controller response");
     });
 
+    router.get('/:id', (Request r) {
+      return Response.ok("Games :id");
+    });
+
     router.all('/<ignored|.*>', (Request request) => Response.notFound('null'));
 
     return router;
